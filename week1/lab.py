02dfc5fb3d0576cd9c3b0056f4ca5da072d8c5e9
Leonardo Lambert <leonardolambert@Leonardos-MacBook-Air.local>
2019-12-02 22:33:34 +0100

    rge

--- week1/lab.py
+++ week1/lab.py
@@ -1,4 +1,17 @@
 from fun_folder.test import miles
 
 print(miles(3,9))
-print("hi")
+
+# 1.1 Part A
+
+print("This program calculates Celsius")
+
+temperature_in_fahrenheit = input("Enter temperature in Farenheit:")
+temperature_in_fahrenheit = float (temperature_in_fahrenheit)
+
+temperature_in_celsius = float ((temperature_in_fahrenheit) - 32) * 5/9
+print("Temperature in Celsius", temperature_in_celsius)
+
+
+
+
--- week1/worksheet.py
+++ week1/worksheet.py
@@ -5,6 +5,7 @@ x = sin(0) + cos(0)
 
 # Question 1: Write a line of code that will print your name.
 print("Leo")
+
 # Question 2: How do you enter a comment in a program?
 # You write a hashtag
 print (3+4)
@@ -83,3 +84,5 @@ miles_driven = input ("Enter miles driven:")
 gallons_used = input ("Enter gallons used:")
 x = miles_driven + gallons_used
 print ("Sum of m + g:", x)
+x = 10
+print("x")
