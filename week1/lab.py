eea6c1a595120da093f172653b29a4ba278e4361
Leonardo Lambert <leonardolambert@Leonardos-MacBook-Air.local>
2019-12-02 22:55:43 +0100

    rg

--- week1/lab.py
+++ week1/lab.py
@@ -13,5 +13,22 @@ temperature_in_celsius = float ((temperature_in_fahrenheit) - 32) * 5/9
 print("Temperature in Celsius", temperature_in_celsius)
 
 
+# 1.2 Part B
+
+print("This Program calculates the Area of a Trapezoid")
+
+trapezoid_height = input("Enter height:")
+trapezoid_height = float(trapezoid_height)
+trapezoid_bottom_base_length = input("Enter bottom base length:")
+trapezoid_bottom_base_length = float(trapezoid_bottom_base_length)
+trapezoid_top_base_length = input("Enter top base length:")
+trapezoid_top_base_length = float(trapezoid_top_base_length)
+
+area = float ((trapezoid_top_base_length + trapezoid_bottom_base_length) / 2) * trapezoid_height
+print("The area is:", area)
+
+# 1.3 Part C
+
+print("This Program calculates the Area of a Circle")
 
 
